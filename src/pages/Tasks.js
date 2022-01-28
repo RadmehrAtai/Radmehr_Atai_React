798bfbb7e14a76b485e83e316c84b79554626ccf
RadmehrAtai <92088576+RadmehrAtai@users.noreply.github.com>
2022-01-28 20:24:04 +0330

    Update Tasks.js

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -1,44 +1,44 @@
 import "../styles/Tasks.css";
-import {getDict} from "../utils/array";
+import {getDict} from "../utils/Array";
 import {useState} from "react";
-import Status from "../utils/status";
+import Status from "../utils/Status";
 import Cards from "../components/Cards";
 
 const Tasks = () => {
     const [cardsList, setCardsList] = useState(getDict());
 
     const edit = (id) => {
-        let updatedItem = null;
-        const newCardsList = cardsList.map((card) => {
-            if (card.id === id) {
-                if (card.status === Status.TODO) {
-                    updatedItem = {
-                        ...card,
-                        status: Status.IN_PROGRESS,
-                    };
-                } else if (card.status === Status.IN_PROGRESS) {
-                    // ToDO
-                } else if (card.status === Status.BLOCKED) {
-                    updatedItem = {
-                        ...card,
-                        status: Status.TODO,
-                    };
-                } else if (card.status === Status.IN_QA) {
-                    updatedItem = {
-                        ...card,
-                        status: Status.DONE,
-                    };
-                } else if (card.status === Status.DONE) {
-                    updatedItem = {
-                        ...card,
-                        status: Status.DEPLOYED,
-                    };
-                }
-                return updatedItem;
-            }
-            return card;
-        });
-        setCardsList(newCardsList);
+        // let updatedItem = null;
+        // const newCardsList = cardsList.map((card) => {
+        //     if (card.id === id) {
+        //         if (card.status === Status.TODO) {
+        //             updatedItem = {
+        //                 ...card,
+        //                 status: Status.IN_PROGRESS,
+        //             };
+        //         } else if (card.status === Status.IN_PROGRESS) {
+        //             // ToDO
+        //         } else if (card.status === Status.BLOCKED) {
+        //             updatedItem = {
+        //                 ...card,
+        //                 status: Status.TODO,
+        //             };
+        //         } else if (card.status === Status.IN_QA) {
+        //             updatedItem = {
+        //                 ...card,
+        //                 status: Status.DONE,
+        //             };
+        //         } else if (card.status === Status.DONE) {
+        //             updatedItem = {
+        //                 ...card,
+        //                 status: Status.DEPLOYED,
+        //             };
+        //         }
+        //         return updatedItem;
+        //     }
+        //     return card;
+        // });
+        // setCardsList(newCardsList);
     }
 
     return (
