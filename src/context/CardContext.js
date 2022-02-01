02d61d5a4f9d74efc84847cd51a12bac4ca175bc
RadmehrAtai <92088576+RadmehrAtai@users.noreply.github.com>
2022-02-01 22:20:31 +0330

    Update

--- src/context/CardContext.js
+++ src/context/CardContext.js
@@ -1,20 +1,50 @@
 import {createContext, useEffect, useState} from "react";
 import {getCards, updateCard} from "../utils/Array";
+import Status from "../utils/Status";
 
 export const CardContext = createContext();
 
 const ToDoApp = ({children}) => {
-    const [cardsList, setCardsList] = useState([]);
-
-    useEffect(() => {
-        setCardsList(getCards());
-    }, [useEffect]);
+    const [cardsList, setCardsList] = useState(getCards);
 
     const editHandler = (id, e) => {
         e.preventDefault();
-        setCardsList(updateCard(id));
-        localStorage.setItem("Cards", JSON.stringify(cardsList));
+        let updatedItem = null;
+        const newCardsList = cardsList.map((card) => {
+            if (card.id === id) {
+                if (card.status === Status.TODO) {
+                    updatedItem = {
+                        ...card,
+                        status: Status.IN_PROGRESS,
+                    };
+                } else if (card.status === Status.IN_PROGRESS) {
+                    // ToDO
+                } else if (card.status === Status.BLOCKED) {
+                    updatedItem = {
+                        ...card,
+                        status: Status.TODO,
+                    };
+                } else if (card.status === Status.IN_QA) {
+                    updatedItem = {
+                        ...card,
+                        status: Status.DONE,
+                    };
+                } else if (card.status === Status.DONE) {
+                    updatedItem = {
+                        ...card,
+                        status: Status.DEPLOYED,
+                    };
+                }
+                return updatedItem;
+            }
+            return card;
+        });
+        setCardsList(newCardsList)
     };
+
+    useEffect(() => {
+        localStorage.setItem("Cards", JSON.stringify(cardsList));
+    }, [editHandler]);
 
     return (
         <CardContext.Provider
--- src/utils/Array.js
+++ src/utils/Array.js
@@ -1,5 +1,3 @@
-import Status from "./Status";
-
 var arr = [];
 let id = 1;
 
@@ -16,38 +14,4 @@ export function addCard(title, desc, status) {
 
 export function getCards() {
     return JSON.parse(localStorage.getItem("Cards"));
-}
-
-export function updateCard(id) {
-    let updatedItem = null;
-    const newCardsList = arr.map((card) => {
-        if (card.id === id) {
-            if (card.status === Status.TODO) {
-                updatedItem = {
-                    ...card,
-                    status: Status.IN_PROGRESS,
-                };
-            } else if (card.status === Status.IN_PROGRESS) {
-                // ToDO
-            } else if (card.status === Status.BLOCKED) {
-                updatedItem = {
-                    ...card,
-                    status: Status.TODO,
-                };
-            } else if (card.status === Status.IN_QA) {
-                updatedItem = {
-                    ...card,
-                    status: Status.DONE,
-                };
-            } else if (card.status === Status.DONE) {
-                updatedItem = {
-                    ...card,
-                    status: Status.DEPLOYED,
-                };
-            }
-            return updatedItem;
-        }
-        return card;
-    });
-    return newCardsList;
 }
